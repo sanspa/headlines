216d648032c50500d116a9ee8bdc05712d7a48ea
Budi Santoso <budi1173@gmail.com>
2016-11-16 11:13:22 +0700

    dynamic route 16/11/16 11:13 Budi Santoso

--- headlines.py
+++ headlines.py
@@ -11,27 +11,19 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
+@app.route("/<publication>")
 
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
-
-
-def get_news(publication):
+def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
      <body>
-       <h1> Headlines </h1>
+       <h1>  Headlines </h1>
        <b>{0}</b> <br/>
        <i>{1}</i> <br/> 
        <p>{2}</p> <br/>
     </body>
-    </html>""".format(first_article.get("title"), first_article.
-get("published"), first_article.get("summary"))
+    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
