b857d23becbd159362f10b31414850176967af26
Budi Santoso <budi1173@gmail.com>
2016-11-18 09:19:28 +0700

    Using weather map

--- headlines.py
+++ headlines.py
@@ -1,4 +1,6 @@
 import feedparser
+import json
+import urllib
 
 from flask import Flask
 from flask import render_template
@@ -21,7 +23,21 @@ def get_news():
         publication = query.lower()
         
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html",articles=feed['entries'])
+    weather = get_weather("London,UK")
+    return render_template("home.html",articles=feed['entries'],weather=weather)
+
+def get_weather(query):
+    api_url = http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fa65bc89d9c04051e54e46bfd17b6917
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    if parsed.get("weather"):
+        weather = {"description":
+                       parsed["weather"][0]["description"],
+                       "temperature":parsed["main"]["temp"],
+                       "city":parsed["name"]}
+    return weather
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
